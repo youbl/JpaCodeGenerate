ab3f707b458e3fc8ea5345e1d0f597bf2c3b885b
youbl <youbl@126.com>
2022-02-09 16:32:28 +0800

    nacos配置对比：增加忽略和恢复某些key功能

--- code-generate/src/main/java/cn/beinet/codegenerate/controller/diff/NacosController.java
+++ code-generate/src/main/java/cn/beinet/codegenerate/controller/diff/NacosController.java
@@ -1,21 +1,19 @@
 package cn.beinet.codegenerate.controller.diff;
 
 import cn.beinet.codegenerate.service.NacosService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.RequiredArgsConstructor;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Properties;
 
 @RestController
+@RequiredArgsConstructor
 public class NacosController {
     private final NacosService nacosService;
-
-    public NacosController(NacosService nacosService) {
-        this.nacosService = nacosService;
-    }
-
+    private final JdbcTemplate jdbcTemplate;
 
     // 获取命名空间列表
     @GetMapping("v1/nacos/namespaces")
@@ -45,9 +43,47 @@ public class NacosController {
         return nacosService.parseYmlToKV(ymlStr, true);
     }
 
-    // 没有app的概念了，不方便忽略
-//    @GetMapping("nacos/ignore")
-//    public void ignoreCompare(@RequestParam String key) {
-//
-//    }
+    // 忽略指定的key差异，或取消忽略
+    @GetMapping("v1/nacos/ignore")
+    public List<String> ignoreCompareGet(@RequestParam String app) {
+        String sql = "select `key` from configIgnoreKeys where app_key='" +
+                formatSqlVal(app) + "'";
+        return jdbcTemplate.queryForList(sql, String.class);
+    }
+
+    // 添加 【忽略指定的key差异】
+    @PostMapping("v1/nacos/ignore")
+    public void ignoreCompareAdd(@RequestParam String app, @RequestParam String key) {
+        String sql = "INSERT INTO configIgnoreKeys (app_key,`key`)VALUES('" +
+                formatSqlVal(app) + "','" + formatSqlVal(key) + "')";
+        jdbcTemplate.execute(sql);
+    }
+
+    // 删除 【忽略指定的key差异】
+    @DeleteMapping("v1/nacos/ignore")
+    public void ignoreCompareDel(@RequestParam String app, @RequestParam String key) {
+        String sql = "delete from configIgnoreKeys where app_key='" +
+                formatSqlVal(app) + "' and `key`='" + formatSqlVal(key) + "'";
+        jdbcTemplate.execute(sql);
+    }
+
+    // 替换单引号
+    private String formatSqlVal(String val) {
+        if (StringUtils.isEmpty(val)) {
+            return val;
+        }
+        return val.trim().replaceAll("'", "");
+    }
 }
+/*
+
+CREATE TABLE `configIgnoreKeys` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT COMMENT '自增ID',
+  `app_key` VARCHAR(1000) NOT NULL DEFAULT '' COMMENT 'app标识',
+  `key` VARCHAR(200) NOT NULL DEFAULT '' COMMENT 'app密钥',
+  `create_date` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  PRIMARY KEY (`id`),
+  KEY `idx_appkey` (`app_key`(768))
+) ENGINE=INNODB DEFAULT CHARSET=utf8mb4 COMMENT='配置忽略信息表'
+
+* */
