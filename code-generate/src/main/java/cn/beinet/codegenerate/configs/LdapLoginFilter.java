b052c894656e197ff465e6a534e673f115592a23
youbl <youbl@126.com>
2022-02-17 10:41:06 +0800

    全局登录页支持

--- code-generate/src/main/java/cn/beinet/codegenerate/configs/LdapLoginFilter.java
+++ code-generate/src/main/java/cn/beinet/codegenerate/configs/LdapLoginFilter.java
@@ -33,51 +33,91 @@ public class LdapLoginFilter extends OncePerRequestFilter {
     // token校验md5的盐值
     private static final String TOKEN_SALT = "beinet.cn";
 
-    private static final String TOKEN_COOKIE_NAME = "zxuser";
+    private static final String TOKEN_COOKIE_NAME = "beinetUser";
     private static final String TOKEN_SPLIT = ":";
 
-    private static final String USER_PARA = "zxuser";
-    private static final String PWD_PARA = "zxpwd";
+    private static final String USER_PARA = "beinetUser";
+    private static final String PWD_PARA = "beinetPwd";
 
-    static Pattern patternRequest = Pattern.compile("(?i)^/actuator/?|\\.(ico|jpg|png|bmp|txt|xml|js|css|ttf|woff|map)$");// |html?
+    private static final String loginPage = "login.html";
+    private static final String loginActionPage = "login";
+    private static final Pattern patternRequest = Pattern.compile("(?i)^/actuator/?|\\.(ico|jpg|png|bmp|txt|xml|js|css|ttf|woff|map)$");// |html?
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        if (isNotApiRequest(request)) {//!logger.isDebugEnabled() ||
+        String url = request.getRequestURI(); //request.getRequestURL() 带有域名，所以不用
+
+        // 验证用户名密码
+        if (url.endsWith(loginActionPage)) {
+            processLoginRequest(request, response);
+            return;
+        }
+
+        // 不需要登录的页面
+        if (!needValidation(url)) {//!logger.isDebugEnabled() ||
             filterChain.doFilter(request, response);
             return;
         }
 
+        // 判断cookie是否有效
         String token = RequestHelper.getCookie(TOKEN_COOKIE_NAME, request);
         if (!validateToken(token)) {
-            String username = request.getParameter(USER_PARA);
-            String pwd = request.getParameter(PWD_PARA);
-            if (!validateUser(username, pwd)) {
-                //throw new RuntimeException("账号或密码错误");
-                endResponse(response, "账号或密码错误");
-                return;
+            addToken("", response);
+            if (StringUtils.isEmpty(token)) {
+                endResponse(response, "未登录");
+            } else {
+                endResponse(response, "token无效");
             }
-
-            addToken(username, response);
+            return;
         }
 
         filterChain.doFilter(request, response);
     }
 
 
-    private boolean isNotApiRequest(HttpServletRequest request) {
-        String url = request.getRequestURI(); //request.getRequestURL() 带有域名，所以不用
+    private boolean needValidation(String url) {
+        // 登录页跳过
+        if (url.endsWith(loginPage))
+            return false;
+
         Matcher matcher = patternRequest.matcher(url);
-        return matcher.find();
+        return !matcher.find();
+    }
+
+    private void processLoginRequest(HttpServletRequest request, HttpServletResponse response) {
+        String username = request.getParameter(USER_PARA);
+        String pwd = request.getParameter(PWD_PARA);
+        if (!validateUser(username, pwd)) {
+            //throw new RuntimeException("账号或密码错误");
+            addToken("", response);
+            endResponse(response, "账号或密码错误");
+            return;
+        }
+
+        addToken(username, response);
+        redirect(request, response);
+    }
+
+    private void redirect(HttpServletRequest request, HttpServletResponse response) {
+        String url = request.getRequestURI(); //request.getRequestURL() 带有域名，所以不用
+        if (url.endsWith(loginActionPage)) {
+            response.setStatus(302);
+            response.setHeader("Location", "index.html");//设置新请求的URL
+        }
     }
 
     private void addToken(String username, HttpServletResponse response) {
         String now = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMddHHmmss"));
-        String token = buildToken(username, now);
 
-        Cookie loginCookie = new Cookie(TOKEN_COOKIE_NAME, token);
+        Cookie loginCookie = new Cookie(TOKEN_COOKIE_NAME, "");
         loginCookie.setPath("/");
-        loginCookie.setMaxAge(7 * 24 * 3600);
+        if (username.isEmpty()) {
+            loginCookie.setMaxAge(0);
+        } else {
+            loginCookie.setMaxAge(7 * 24 * 3600);
+            String token = buildToken(username, now);
+            loginCookie.setValue(token);
+        }
         response.addCookie(loginCookie);
     }
 
@@ -85,8 +125,9 @@ public class LdapLoginFilter extends OncePerRequestFilter {
         if (StringUtils.isEmpty(token)) {
             return false;
         }
+        // 0为用户名，1为时间，2为md5
         String[] arr = token.split(TOKEN_SPLIT);
-        if (arr.length != 3) {
+        if (arr.length != 3 || arr[0].isEmpty() || arr[1].isEmpty()) {
             return false;
         }
         String countToken = buildToken(arr[0], arr[1]);
@@ -118,7 +159,7 @@ public class LdapLoginFilter extends OncePerRequestFilter {
     private void endResponse(HttpServletResponse response, String msg) {
         response.setContentType("application/json; charset=UTF-8");
         try {
-            String ret = "{'ret':500, 'msg':'" + msg.replaceAll("'", "") + "'}";
+            String ret = "{\"ret\":500, \"msg\":\"" + msg.replaceAll("[\"']", "") + "\"}";
             response.getOutputStream().write(ret.getBytes(StandardCharsets.UTF_8));
         } catch (IOException e) {
             e.printStackTrace();
