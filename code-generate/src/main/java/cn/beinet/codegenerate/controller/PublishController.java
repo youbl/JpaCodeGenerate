b2bb8adf6c458658022aa4b2aaffe5cff20de113
beiliang_you <beiliang_you@fzzixun.com>
2023-01-04 16:21:03 +0800

    增加发布上传支持接口

---
+++ code-generate/src/main/java/cn/beinet/codegenerate/controller/PublishController.java
@@ -0,0 +1,72 @@
+package cn.beinet.codegenerate.controller;
+
+import cn.beinet.codegenerate.configs.AuthDetails;
+import cn.beinet.codegenerate.util.ProcessHelper;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 接收文件上传，和服务重启。
+ * 用于部署在某台单机上的服务发布
+ *
+ * @author youbl
+ * @date 2023/1/4 10:48
+ */
+@Slf4j
+@RestController
+public class PublishController {
+    private final Map<String, String> serviceDir;
+
+    public PublishController() {
+        // key为服务名，value为对应文件路径
+        serviceDir = new HashMap<>();
+        serviceDir.put("rpaopsTest", "/data/app/jars/test-scheduler-ops-0.0.1-SNAPSHOT.jar");
+        serviceDir.put("rpaopsPrev", "/data/app/jars/prev-scheduler-ops-0.0.1-SNAPSHOT.jar");
+        serviceDir.put("rpaops", "/data/app/jars/prod-scheduler-ops-0.0.1-SNAPSHOT.jar");
+
+        serviceDir.put("testdebugTool", "/data/app/jars_debugTool/testdebugTool.jar");
+        serviceDir.put("debugTool", "/data/app/jars_debugTool/debugTool.jar");
+
+        serviceDir.put("sqlCompare", "/data/app/jars_sqlCompare/code-generate-0.0.1-SNAPSHOT.jar");
+    }
+
+    // 上传文件
+    @PostMapping("publish/upload/{serviceName}")
+    public String upload(@RequestParam("file") MultipartFile file,
+                         @PathVariable String serviceName,
+                         AuthDetails loginInfo) throws IOException {
+        if (file == null || file.isEmpty()) {
+            return "文件内容为空";
+        }
+        String savePath = serviceDir.get(serviceName);
+        if (savePath == null) {
+            String errmsg = "不支持的服务:" + serviceName;
+            log.error(errmsg);
+            return errmsg;
+        }
+        file.transferTo(new File(savePath));
+        log.info("{} 上传文件 {} 存入 {}", loginInfo.getAccount(), serviceName, savePath);
+        return "OK:" + savePath;
+    }
+
+
+    // 重启指定服务
+    @PostMapping("publish/{serviceName}")
+    public String publish(@PathVariable String serviceName, AuthDetails loginInfo) {
+        String command = "/bin/supervisorctl restart '" + serviceName + "'";
+        log.info("{} 执行命令 {} ", loginInfo.getAccount(), command);
+        // String result = ProcessHelper.run("sh", "-c", command);
+        String result = ProcessHelper.run(command);//("/bin/supervisorctl", "restart", serviceName);
+        log.info("{} 执行命令 {} 结果 {}", loginInfo.getAccount(), command, result);
+        return result;
+    }
+}
---
+++ code-generate/src/main/java/cn/beinet/codegenerate/util/ProcessHelper.java
@@ -0,0 +1,68 @@
+package cn.beinet.codegenerate.util;
+
+import org.springframework.util.StringUtils;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 新类
+ *
+ * @author youbl
+ * @date 2023/1/4 11:35
+ */
+public final class ProcessHelper {
+
+    /**
+     * 执行shell命令，并读取返回内容
+     *
+     * @param command
+     * @param arguments
+     * @return
+     */
+    public static String run(String command, String... arguments) {
+        List<String> commands = new ArrayList<>();
+        commands.add(command);
+        if (arguments != null) {
+            for (String item : arguments)
+                commands.add(item);
+        }
+        //Runtime.getRuntime().exec(commands);
+        ProcessBuilder processBuilder = new ProcessBuilder(commands);
+        try {
+            Process process = processBuilder.start();
+            String outputInfo = readInputStream(process.getInputStream());
+            String outputErr = readInputStream(process.getErrorStream());
+            if (StringUtils.hasLength(outputErr)) {
+                outputInfo += "\nError: " + outputErr;
+            }
+            return outputInfo;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * 读取输入流里的字符串信息
+     *
+     * @param stream 流
+     * @return string
+     */
+    private static String readInputStream(InputStream stream) {
+        try {
+            BufferedReader stdInput = new BufferedReader(new InputStreamReader(stream));
+            StringBuilder stringBuilder = new StringBuilder();
+            String line;
+            while ((line = stdInput.readLine()) != null) {
+                stringBuilder.append(line).append("\n");
+            }
+            return stringBuilder.toString();
+        } catch (Exception exp) {
+            throw new RuntimeException(exp);
+        }
+    }
+}
