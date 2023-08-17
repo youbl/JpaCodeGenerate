345e49a78c02c81c337cec2bf6d62dfefcafacbc
beiliang_you <youbl@126.com>
2023-08-17 10:50:25 +0800

    OTPCode入库进行加密，取出使用进行解密

--- code-generate/src/main/java/cn/beinet/codegenerate/otpcode/service/OtpCodeService.java
+++ code-generate/src/main/java/cn/beinet/codegenerate/otpcode/service/OtpCodeService.java
@@ -2,10 +2,12 @@ package cn.beinet.codegenerate.otpcode.service;
 
 import cn.beinet.codegenerate.otpcode.controller.dto.OtpCodeDto;
 import cn.beinet.codegenerate.otpcode.service.entity.OtpCode;
+import cn.beinet.codegenerate.util.AESUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -34,12 +36,16 @@ public class OtpCodeService {
 
         List<OtpCodeDto> ret = new ArrayList<>(codes.size());
         for (OtpCode item : codes) {
-            String code = OtpCodeGeneratorTool.countCodeStr(item.getSecure());
+            // 解密密钥后，进行code计算生成
+            String descryptedSecure = descrypt(item.getSecure());
+            String code = OtpCodeGeneratorTool.countCodeStr(descryptedSecure);
+
             OtpCodeDto dto = new OtpCodeDto()
                     .setCode(code)
                     .setCreateTime(item.getCreate_time())
                     .setTitle(item.getTitle())
-                    .setUsername(item.getUsername());
+                    .setUsername(item.getUsername())
+                    .setSecure(""); // 密钥默认不返回
             ret.add(dto);
         }
         return ret;
@@ -52,11 +58,13 @@ public class OtpCodeService {
      * @return 影响行数
      */
     public int saveOtpCode(OtpCodeDto dto) {
+        String encryptedPwd = encrypt(dto.getSecure());
+
         String insertSql = "INSERT INTO otpcode (username,title,secure)VALUES(?,?,?)";
         return jdbcTemplate.update(insertSql, new Object[]{
                 dto.getUsername(),
                 dto.getTitle(),
-                dto.getSecure()
+                encryptedPwd,
         });
     }
 
@@ -71,5 +79,20 @@ public class OtpCodeService {
         // List<Map<String, Object>> codes = jdbcTemplate.queryForList(sql, new Object[]{username});
         List<OtpCode> codes = jdbcTemplate.query(sql, new Object[]{username}, new BeanPropertyRowMapper<>(OtpCode.class));
         return codes;
+    }
+
+
+    // 入库前要加密
+    private String encrypt(String sourceStr) {
+        if (StringUtils.hasLength(sourceStr))
+            return AESUtil.encrypt(sourceStr);
+        return sourceStr;
+    }
+
+    // 出库后要解密
+    private String descrypt(String encryptedStr) {
+        if (StringUtils.hasLength(encryptedStr))
+            return AESUtil.decrypt(encryptedStr);
+        return encryptedStr;
     }
 }
