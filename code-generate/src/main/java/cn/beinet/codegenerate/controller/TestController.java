9a70e5bc94ff33497bce39160d209b9341ea8bb5
beiliang_you <youbl@126.com>
2023-10-16 18:29:48 +0800

    增加test控制器，排查问题

---
+++ code-generate/src/main/java/cn/beinet/codegenerate/controller/TestController.java
@@ -0,0 +1,49 @@
+package cn.beinet.codegenerate.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Enumeration;
+
+/**
+ * 新类
+ *
+ * @author youbl
+ * @since 2023/10/16 15:20
+ */
+@RestController
+public class TestController {
+    @GetMapping(value = "test", produces = {"text/plain"})
+    public String test(HttpServletRequest request) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(request.getMethod())
+                .append(" ")
+                .append(request.getRequestURL())
+                .append("\r\n\n");
+        sb.append("IP-addr: ")
+                .append(request.getRemoteAddr())
+                .append(" ")
+                .append(request.getRemoteHost())
+                .append(" ")
+                .append(request.getRemotePort())
+                .append(" ")
+                .append(request.getRemoteUser())
+                .append("\r\n\n");
+
+        Enumeration<String> allHeader = request.getHeaderNames();
+        while (allHeader.hasMoreElements()) {
+            String header = allHeader.nextElement();
+            Enumeration<String> vals = request.getHeaders(header);
+            while (vals.hasMoreElements()) {
+                String val = vals.nextElement();
+                sb.append(header)
+                        .append(" : ")
+                        .append(val)
+                        .append("\r\n");
+            }
+        }
+
+        return sb.toString();
+    }
+}
