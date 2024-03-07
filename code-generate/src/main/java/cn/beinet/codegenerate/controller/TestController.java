35ba039ce80f4f08b8017e5b3483b69017944b4b
beiliang_you <youbl@126.com>
2024-03-07 16:36:11 +0800

    增加客户端IP响应接口，增加本地IP和后端出口IP响应接口，增加telnet检测接口

--- code-generate/src/main/java/cn/beinet/codegenerate/controller/TestController.java
+++ code-generate/src/main/java/cn/beinet/codegenerate/controller/TestController.java
@@ -1,6 +1,11 @@
 package cn.beinet.codegenerate.controller;
 
+import cn.beinet.codegenerate.util.IpHelper;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
@@ -13,7 +18,11 @@ import java.util.Enumeration;
  * @since 2023/10/16 15:20
  */
 @RestController
+@Slf4j
 public class TestController {
+    @Value("${getip-url}")
+    private String getIpUrl;
+
     @GetMapping(value = "test", produces = {"text/plain"})
     public String test(HttpServletRequest request) {
         StringBuilder sb = new StringBuilder();
@@ -45,5 +54,49 @@ public class TestController {
         }
 
         return sb.toString();
+    }
+
+    @GetMapping("test/loop")
+    @SneakyThrows
+    public String testLoop() {
+        int loopTime = 1000;
+        while (loopTime > 0) {
+            log.info(loopTime + " looping...");
+            loopTime--;
+            Thread.sleep(10000);
+        }
+        return "ok";
+    }
+
+    @GetMapping("test/ip")
+    public String testShowClientIp(HttpServletRequest request) {
+        return IpHelper.getClientIp(request);
+    }
+
+    @GetMapping("test/sip")
+    public String testShowServerIp(HttpServletRequest request) {
+        return "local:[" + IpHelper.getServerIp() + "] outer:[" + IpHelper.getOuterIp(getIpUrl) + "]";
+    }
+
+    /**
+     * 在本机测试与远端IP的网络连接性，模拟Telnet
+     *
+     * @param ip      远端IP
+     * @param port    远端端口
+     * @param timeout 超时时间，默认5000毫秒
+     * @return 测试结果
+     */
+    @GetMapping("test/telnet")
+    @SneakyThrows
+    public String testTelnet(@RequestParam String ip, @RequestParam int port, @RequestParam(required = false) Integer timeout) {
+        String ret = ip + ":" + port + " 连接";
+        long start = System.currentTimeMillis();
+        try {
+            IpHelper.telnet(ip, port, timeout);
+            ret += "成功";
+        } catch (Exception exp) {
+            ret += "失败:" + exp.getMessage();
+        }
+        return ret + " 耗时:" + (System.currentTimeMillis() - start) + "ms";
     }
 }
---
+++ code-generate/src/main/java/cn/beinet/codegenerate/util/IpHelper.java
@@ -0,0 +1,150 @@
+package cn.beinet.codegenerate.util;
+
+import lombok.SneakyThrows;
+import org.springframework.util.StringUtils;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.net.SocketAddress;
+import java.net.URL;
+import java.net.UnknownHostException;
+
+/**
+ * Description:
+ * IP相关工具方法
+ *
+ * @author : youbl
+ * @create: 2021/9/15 20:57
+ */
+public class IpHelper {
+
+    /**
+     * 获取本机网卡IP
+     *
+     * @return 本机网卡IP
+     */
+    public static String getServerIp() {
+        try {
+            InetAddress address = InetAddress.getLocalHost();
+            return address.getHostAddress();
+        } catch (UnknownHostException e) {
+            return e.getMessage();
+        }
+    }
+
+    /**
+     * 获取本机的公网出口IP
+     *
+     * @param ipUrl 检测出口IP的地址，可空
+     * @return 公网出口IP
+     */
+    public static String getOuterIp(String ipUrl) {
+        if (!StringUtils.hasLength(ipUrl)) {
+            ipUrl = "https://www.taobao.com/help/getip.php";
+        }
+        HttpURLConnection conn = null;
+        try {
+            conn = (HttpURLConnection) (new URL(ipUrl).openConnection());
+            conn.setRequestMethod("GET");
+            int responseCode = conn.getResponseCode();
+
+            if (responseCode == HttpURLConnection.HTTP_OK) {
+                try (BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()))) {
+                    String inputLine;
+                    StringBuilder response = new StringBuilder();
+
+                    while ((inputLine = in.readLine()) != null) {
+                        response.append(inputLine);
+                    }
+                    return response.toString();
+                }
+            }
+            return ("响应：" + responseCode);
+        } catch (Exception e) {
+            return e.getMessage();
+        } finally {
+            if (conn != null)
+                conn.disconnect();
+        }
+    }
+
+
+    /**
+     * 获取当前请求上下文里的所有客户端IP
+     *
+     * @param request 请求上下文
+     * @return 客户端IP列表
+     */
+    public static String getClientIp(HttpServletRequest request) {
+        String remoteAddr = request.getRemoteAddr();
+        remoteAddr += getHeader(request, "X-Forwarded-For", "f");
+        remoteAddr += getHeader(request, "X-Real-IP", "r");
+        remoteAddr += getHeader(request, "ali-cdn-real-ip", "d");
+        remoteAddr += getHeader(request, "Proxy-Client-IP", "p");
+        remoteAddr += getHeader(request, "WL-Proxy-Client-IP", "w");
+        return remoteAddr;
+    }
+
+    private static String getHeader(HttpServletRequest request, String header, String prefix) {
+        String realIp = request.getHeader(header);
+        if (StringUtils.hasLength(realIp)) {
+            return ";" + prefix + ":" + realIp;
+        }
+        return "";
+    }
+
+    /**
+     * 按定死的顺序，获取唯一一个客户端请求ip
+     * 注意：因为客户端Header可以伪造，不推荐此方法用于对安全有要求的场景。
+     *
+     * @param request 请求上下文
+     * @return 唯一IP
+     */
+    public static String getIpAddr(HttpServletRequest request) {
+        String ip = request.getHeader("x-forwarded-for");
+        if (ip == null || ip.isEmpty() || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("ali-cdn-real-ip");
+        }
+        if (ip == null || ip.isEmpty() || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("x-real-ip");
+        }
+        if (ip == null || ip.isEmpty() || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("Proxy-Client-IP");
+        }
+        if (ip == null || ip.isEmpty() || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("WL-Proxy-Client-IP");
+        }
+        if (ip == null || ip.isEmpty() || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getRemoteAddr();
+        }
+        return ip.equals("0:0:0:0:0:0:0:1") ? "127.0.0.1" : ip.split(",")[0].trim();
+    }
+
+    /**
+     * 尝试连接指定IP和端口，用于检测网络是否连通
+     *
+     * @param ip      远端IP
+     * @param port    远端端口
+     * @param timeout 超时时间，毫秒，默认5000
+     */
+    @SneakyThrows
+    public static void telnet(String ip, int port, Integer timeout) {
+        if (timeout == null)
+            timeout = 5000;
+
+        try (Socket socket = new Socket()) {
+            SocketAddress address = new InetSocketAddress(ip, port);
+            socket.connect(address, timeout);
+            // socket.setSoTimeout(timeout);
+
+            // 增加休眠代码，可以在cmd里查询端口连接情况： netstat -ano |findstr 对应IP
+            // 休眠期间，会输出：  TCP    10.100.68.142:14290    IP:端口      ESTABLISHED     11556
+            // Thread.sleep(3000);
+        }
+    }
+}
