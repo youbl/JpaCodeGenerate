865d384e382fa1abc2fb0e8442c394c6f890ce67
beiliang_you <youbl@126.com>
2024-04-25 18:57:26 +0800

    增加默认菜单配置

--- code-generate/src/main/java/cn/beinet/codegenerate/menu/MenuController.java
+++ code-generate/src/main/java/cn/beinet/codegenerate/menu/MenuController.java
@@ -3,6 +3,7 @@ package cn.beinet.codegenerate.menu;
 import cn.beinet.codegenerate.menu.dto.MenuGroupDto;
 import cn.beinet.codegenerate.menu.service.MenuService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -21,6 +22,9 @@ import java.util.List;
 public class MenuController {
     private final MenuService menuService;
 
+    @Value("${default-saas:ml}")
+    private String defaultSaasId;
+
     /**
      * 获取指定SaaS ID下，所有的菜单树返回
      *
@@ -29,6 +33,9 @@ public class MenuController {
      */
     @GetMapping("/menuGroup")
     public List<MenuGroupDto> findAllForShow(@RequestParam(required = false) String saasId) {
+        if (!StringUtils.hasLength(saasId)) {
+            saasId = defaultSaasId;
+        }
         if (!StringUtils.hasLength(saasId)) {
             saasId = "ml";
         }
