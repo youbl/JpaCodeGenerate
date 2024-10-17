e24c7b0b859765dc6dd07ce1d27133a59df77729
beiliang_you <youbl@126.com>
2024-10-17 19:22:49 +0800

    添加otp secret key的生成方法，并适配iphone和android的google authenticator应用

--- code-generate/src/main/java/cn/beinet/codegenerate/otpcode/service/OtpCodeGeneratorTool.java
+++ code-generate/src/main/java/cn/beinet/codegenerate/otpcode/service/OtpCodeGeneratorTool.java
@@ -6,10 +6,9 @@ import org.springframework.util.StringUtils;
 
 import javax.crypto.Mac;
 import javax.crypto.spec.SecretKeySpec;
-import java.time.Instant;
-import java.time.LocalDateTime;
-import java.time.ZoneId;
-import java.time.format.DateTimeFormatter;
+import java.nio.charset.StandardCharsets;
+import java.security.SecureRandom;
+import java.util.Base64;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -17,7 +16,49 @@ import java.util.Map;
  * 2FA使用的OTP（one time password）生成辅助工具类
  */
 public class OtpCodeGeneratorTool {
-    static long second_per_size = 30L;// 每次时间长度，默认30秒
+    private static final long second_per_size = 30L;// 每次时间长度，默认30秒
+    private static final String SALT = ".beinet.cn";
+
+    /**
+     * 生成一个16位的Base32字符组成的SecretKey，用于外部绑定到用户.
+     * 注1：Android的google authenticator只支持Base32字符，不限制长度。
+     * 注2： iPhone的google authenticator只支持16位长度的Base32字符。
+     *
+     * @return SecretKey
+     */
+    @SneakyThrows
+    public static String generateSecretKey() {
+        SecureRandom sr = SecureRandom.getInstance("SHA1PRNG");
+        sr.setSeed(getSeed());
+        byte[] buffer = sr.generateSeed(64);
+        String ret = Base64.getEncoder().encodeToString(buffer);
+        // ret = ret.replaceAll("[\\\\=+\\s/]", "");// 移除末尾的等号
+
+        // google authenticator导入报错：密钥无法识别，因为google authenticator期望secret是有效的Base32编码
+        // Base32 编码使用的字符集限于 A-Z 和 2-7
+        ret = ret.replaceAll("[^A-Z2-7]", "")
+                // .replaceAll("(.{8})", "$1 ") 每8个字符后加1个空格
+                .trim();
+
+        // iPhone手机要求长度必须是16位
+        if (ret.length() < 16) {
+            // 将ret长度补足16位
+            StringBuilder sb = new StringBuilder(ret);
+            while (sb.length() < 16) {
+                sb.append(ret);
+            }
+            ret = sb.toString();
+        }
+        if (ret.length() > 16) {
+            ret = ret.substring(0, 16);
+        }
+        return ret;
+    }
+
+    private static byte[] getSeed() {
+        String str = SALT + System.currentTimeMillis() + SALT;
+        return str.getBytes(StandardCharsets.UTF_8);
+    }
 
     /**
      * 生成二维码所需的字符串，注：这个format不可修改，否则会导致身份验证器无法识别二维码
@@ -32,13 +73,23 @@ public class OtpCodeGeneratorTool {
             title = title.replaceAll(":", ""); // 不允许包含冒号
             user = title + ":" + user;
         }
-
         String format = "otpauth://totp/%s?secret=%s";
         String ret = String.format(format, user, secret);
 //        if (StringUtils.hasLength(title)) {
 //            ret += "&issuer=" + title;
 //        }
         return ret;
+    }
+
+    /**
+     * 获取指定密钥，当前时间的otpCode
+     *
+     * @param secret 用户绑定的secretKey
+     * @return 生成的与时间关联的code
+     */
+    public static String countCodeStr(String secret) {
+        long timeSecond = System.currentTimeMillis() / 1000L / second_per_size;
+        return countCode(secret, timeSecond);
     }
 
     /**
@@ -69,21 +120,16 @@ public class OtpCodeGeneratorTool {
         return ret;
     }
 
-    private static String tsToDateStr(long timeSecond) {
-        Instant instant = Instant.ofEpochSecond(timeSecond);
-        ZoneId zoneId = ZoneId.of("Asia/Shanghai");
-        LocalDateTime localDateTime = instant.atZone(zoneId).toLocalDateTime();
-        return localDateTime.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
-    }
-
     /**
      * 获取指定密钥的otpcode
      *
      * @param secret     用户绑定的secretKey
-     * @param timeWindow 时间窗口，截止当前是每30秒一个窗口
+     * @param timeWindow 时间窗口，截止当前是每30秒一个窗口，一般为 秒级时间戳 除以 30
      * @return 生成的当前时间的code
      */
     public static String countCode(String secret, long timeWindow) {
+        if (secret == null || secret.isEmpty())
+            return "";
         byte[] decodedKey = countKey(secret);
         return addZero(verifyCode(decodedKey, timeWindow), 6);
     }
@@ -94,7 +140,7 @@ public class OtpCodeGeneratorTool {
      * @param secret 密钥
      * @return 字节数组
      */
-    public static byte[] countKey(String secret) {
+    private static byte[] countKey(String secret) {
         Base32 codec = new Base32();
         return codec.decode(secret);
     }
@@ -137,7 +183,7 @@ public class OtpCodeGeneratorTool {
 
     private static String addZero(int code, int returnLen) {
         StringBuilder ret = new StringBuilder(String.valueOf(code));
-        for (int i = ret.length(), j = returnLen; i < j; i++) {
+        for (int i = ret.length(); i < returnLen; i++) {
             ret.insert(0, "0");
         }
         return ret.toString();
