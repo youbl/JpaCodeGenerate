81f29c101540f84c1af25a921274386d0e84761c
youbl <youbl@126.com>
2025-07-10 13:45:15 +0800

    配置 RequestContextListener，解决 RequestContextHolder.getRequestAttributes()为null问题。原因暂时不明

--- code-generate/src/main/java/cn/beinet/codegenerate/configs/WebMvcConfig.java
+++ code-generate/src/main/java/cn/beinet/codegenerate/configs/WebMvcConfig.java
@@ -1,7 +1,9 @@
 package cn.beinet.codegenerate.configs;
 
 import cn.beinet.codegenerate.configs.arguments.AuthDetailArgumentResolver;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.context.request.RequestContextListener;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -23,5 +25,13 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(new AuthDetailArgumentResolver());
+    }
+
+    /**
+     * 配置 RequestContextListener，解决 RequestContextHolder.getRequestAttributes()为null问题。原因暂时不明
+     */
+    @Bean
+    public RequestContextListener requestContextListener() {
+        return new RequestContextListener();
     }
 }
